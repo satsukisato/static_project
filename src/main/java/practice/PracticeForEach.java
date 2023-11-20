b353bec804dc215a31055aa71d93b00c53f0afad
satsuki sato <timothee0034@gmail.com>
2023-11-20 16:46:11 +0900

    static_project commit

---
+++ src/main/java/practice/PracticeForEach.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class PracticeForEach {
+	public static void main(String[] args) {
+		String name[] = { "田原", "福尾", "倉成", "濱野", "前村", "重永", "岸辺", "吉良", "東方", "広瀬", "花京院" };
+		for (int i = 0; i < name.length; i++) {
+			System.out.println(name[i]);
+		}
+	}
+}
---
+++ src/main/java/practice/PracticeSwitch.java
@@ -0,0 +1,33 @@
+package practice;
+
+public class PracticeSwitch {
+	public static void main(String[] args) {
+		int[] numbers = { 1, 2, 4, 5, 3, 2, 1, 2, 5, 1 };
+
+		for (int num : numbers) {
+			String job = "";
+			switch (num) {
+			case 1:
+				job = "勇者";
+				break;
+			case 2:
+				job = "バトルマスター";
+				break;
+			case 3:
+				job = "パラディン";
+				break;
+			case 4:
+				job = "海賊";
+				break;
+			case 5:
+				job = "魔法剣士";
+				break;
+			default:
+				job = "なし";
+				break;
+			}
+			System.out.println(num + ":" + job);
+		}
+	}
+
+}
